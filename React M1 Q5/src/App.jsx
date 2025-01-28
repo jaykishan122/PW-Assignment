df0b6e12cf849022a3445a3a7eb81ae77099b0d8
Jaykishan Sahani <jaykishansahani55@gmail.com>
2025-01-28 11:26:20 +0530

    react

---
+++ React M1 Q5/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import headphoneImage from './assets/headphone.jpg';
+
+
+function App() {
+  return  <div>
+  <h1>Welcome to the React Project</h1>
+  <img src={headphoneImage} alt="React Project Jaykishan" />
+  <h4>Headphones | High Base Clear Sound </h4>
+  <p>The flagship-level battery life for all-new OnePlus Nord Buds 2r  delivers up to 38 hrs of non-stop music on a single charge</p>
+  <p> &#x24; 12</p>
+  <button>Buy Now</button>
+</div>
+}
+
+export default App
