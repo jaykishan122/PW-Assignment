56784c44797ecaa8e5e57341b83ddae77e5848cb
Jaykishan Sahani <jaykishansahani55@gmail.com>
2025-01-28 11:39:17 +0530

    react-form

---
+++ React -M1 Q4/src/Login.jsx
@@ -0,0 +1,12 @@
+export default function Login(){
+ 
+    return (
+        <>
+        <form action="">
+            <input type="text"  placeholder="username"/>
+            <input type="password"  placeholder="password"/>
+            <button>Submit</button>
+        </form>
+        </>
+    )
+}
